1387b681492f47d7ec1ff192b96bffadcdab0c72
Mikhail Goncharov <17141437+sharptrip@users.noreply.github.com>
2022-09-16 09:40:27 +0200

    update fences

--- packages/dds/tree/src/test/feature-libraries/editable-tree/editableTree.edit.spec.ts
+++ packages/dds/tree/src/test/feature-libraries/editable-tree/editableTree.edit.spec.ts
@@ -213,7 +213,7 @@ async function buildTestPerson(): Promise<[EditableTreeContext, PersonType]> {
     return [context, proxy as PersonType];
 }
 
-describe.only("editing with editable-tree", () => {
+describe("editing with editable-tree", () => {
     it("update property", async () => {
         const trees = await setupForest(fullSchemaData, personData);
         const [context, person] = getEditableTree(trees[0]);
